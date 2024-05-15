dce1d3abb34b52451b6134628f4658c32e7279dd
ENDOH takanao <djmchl@gmail.com>
2024-05-15 13:35:14 +0900

    feat: Add server implementation for ogen

---
+++ ogen/cmd/server/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+	"time"
+
+	"github.com/go-faster/errors"
+	"github.com/go-faster/sdk/app"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+
+	"ogen/internal/handler"
+	"ogen/oas"
+)
+
+const shutdownTimeout = 15 * time.Second
+
+func main() {
+	app.Run(func(ctx context.Context, lg *zap.Logger, m *app.Metrics) error {
+		var arg struct {
+			Addr string
+		}
+		flag.StringVar(&arg.Addr, "addr", "0.0.0.0:8080", "listen address")
+		flag.Parse()
+
+		lg.Info("Initializing",
+			zap.String("http.addr", arg.Addr),
+		)
+		oasServer, err := oas.NewServer(
+			handler.Handler{},
+			&SecurityHandler{},
+			oas.WithTracerProvider(m.TracerProvider()),
+			oas.WithMeterProvider(m.MeterProvider()),
+		)
+		if err != nil {
+			return errors.Wrap(err, "server init")
+		}
+
+		httpServer := http.Server{
+			ReadHeaderTimeout: time.Second,
+			Addr:              arg.Addr,
+			Handler:           oasServer,
+		}
+		g, ctx := errgroup.WithContext(ctx)
+		g.Go(func() error {
+			// Wait until g ctx canceled, then try to shut down server.
+			<-ctx.Done()
+
+			lg.Info("Shutting down", zap.Duration("timeout", shutdownTimeout))
+
+			shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+			defer cancel()
+			return httpServer.Shutdown(shutdownCtx)
+		})
+		g.Go(func() error {
+			defer lg.Info("Server stopped")
+			if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				return errors.Wrap(err, "http")
+			}
+			return nil
+		})
+
+		return g.Wait()
+	})
+}
+
+var _ oas.SecurityHandler = (*SecurityHandler)(nil)
+
+type SecurityHandler struct{}
+
+func (*SecurityHandler) HandleApiKeyAuth(ctx context.Context, operationName string, t oas.ApiKeyAuth) (context.Context, error) {
+	return ctx, nil
+}
+
+func (*SecurityHandler) HandleOAuth2(ctx context.Context, operationName string, t oas.OAuth2) (context.Context, error) {
+	return ctx, nil
+}
---
+++ ogen/internal/handler/handler.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/go-faster/sdk/zctx"
+	"go.uber.org/zap"
+
+	"ogen/oas"
+)
+
+// Compile-time check for Handler.
+var _ oas.Handler = (*Handler)(nil)
+
+type Handler struct {
+	oas.UnimplementedHandler // automatically implement all methods
+}
+
+func (h Handler) HealthServiceCheck(ctx context.Context, params oas.HealthServiceCheckParams) (*oas.V1CheckResponse, error) {
+	zctx.From(ctx).Info("HealthServiceCheck", zap.Any("params", params))
+	return &oas.V1CheckResponse{
+		Status: oas.CheckResponseServingStatusSERVINGSTATUSSERVING,
+	}, nil
+}
+
+func (h Handler) ExampleServiceGet(ctx context.Context, req *oas.ExampleServiceQueryBody, params oas.ExampleServiceQueryParams) (*oas.V1ExampleServiceQueryResponse, error) {
+	zctx.From(ctx).Info("ExampleServiceGet", zap.Any("req", req), zap.Any("params", params))
+	return &oas.V1ExampleServiceQueryResponse{
+		Status: oas.OptProtov1Status{
+			Value: oas.Protov1Status{},
+			Set:   true,
+		},
+	}, nil
+}
